fd2b72b91a86f4d54c6f4151cacba604dd62693f
var1a3le <darklight11007@gmail.com>
2021-08-27 17:15:58 +0700

    check end number

--- src/main/java/Range.java
+++ src/main/java/Range.java
@@ -1,10 +1,28 @@
 public class Range {
-    public Range(String value) {
+    public final String text;
 
+    public Range(String value) {
+        this.text = value;
     }
 
     public boolean isStartWithInclusive() {
-        return true;
+        return text.startsWith("[");
+    }
+
+    public int getStartInput() {
+        int startIndex = 1;
+        int lastIndex = 2;
+        return Integer.parseInt(text.substring(startIndex, lastIndex));
+    }
+
+    public boolean isEndWithInclusive() {
+        return text.endsWith("]");
+    }
+
+    public int getEndInput() {
+        int startIndex = text.length() - 2;
+        int lastIndex = text.length() - 1;
+        return Integer.parseInt(text.substring(startIndex, lastIndex));
     }
 
 }
--- src/test/java/RangeTest.java
+++ src/test/java/RangeTest.java
@@ -1,12 +1,47 @@
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RangeTest {
 
     @Test
-    public void isInclusiveWithBucketShouldBeTrue() {
+    public void isStartWithInclusiveWithBucketShouldBeTrue() {
         Range range = new Range("[1,5]");
         boolean actual = range.isStartWithInclusive();
         assertTrue(actual);
+    }
+
+    @Test
+    public void isStartWithInclusiveWithBucketShouldBeFalse() {
+        Range range = new Range("(1,5]");
+        boolean actual = range.isStartWithInclusive();
+        assertFalse(actual);
+    }
+
+    @Test
+    public void getStatInputShouldbe1() {
+        Range range = new Range("(1,5]");
+        int actual = range.getStartInput();
+        assertEquals(1, actual);
+    }
+
+    @Test
+    public void isEndWithInclusiveWithBracketShouldBeTrue() {
+        Range range = new Range("[1,5]");
+        boolean actual = range.isEndWithInclusive();
+        assertTrue(actual);
+    }
+
+    @Test
+    public void isEndWithInclusiveWithOutBracketShouldBeTrue() {
+        Range range = new Range("[1,5)");
+        boolean actual = range.isEndWithInclusive();
+        assertFalse(actual);
+    }
+
+    @Test
+    public void getEndInputShouldbe5() {
+        Range range = new Range("(1,5]");
+        int actual = range.getEndInput();
+        assertEquals(5, actual);
     }
 }
